ce5a69add35af6b490d4571a88511979cc583b49
Karthik Kommaraju <karthikkommaraju1563@gmail.com>
2025-08-01 17:39:58 +0530

    {changes in server and messages}

--- Server/server.js
+++ Server/server.js
@@ -3,18 +3,21 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
-
+// http server
 const app = express();
 const server = http.createServer(app)
 
+// Middleware setup
 app.use(express.json({limit:"4mb"}));
 app.use(cors());
 
+// Routes setup
 app.use("/api/status",(req,res)=>res.send("Server is live"));
+app.use("/api/auth",userRouter);
 
-app.use("/api/auth")
-
+// Connect to db
 await connectDB();
 
 const PORT = process.env.PORT || 5000;
